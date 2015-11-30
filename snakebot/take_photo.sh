e8646a9b042790f5010e96665ed50a9b0fa6f869
James-Firth <hello@james-firth.com>
2015-11-29 22:14:22 -0600

    Added more flags to take_photo. Makes nicer photo now.

--- snakebot/take_photo.sh
+++ snakebot/take_photo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-fswebcam -r 640x480 --jpeg 100 --title "Print Progress :)" --shadow -set "brightness=100%" --save "$HOME/Pictures/3dprinter/photo.jpg";
+fswebcam -r 640x480 --jpeg 100 --title "Print Progress :)" --shadow -set "brightness=100%" --line-colour "#40152982" --top-banner --save "$HOME/Pictures/3dprinter/photo.jpg";
 exit $?;
